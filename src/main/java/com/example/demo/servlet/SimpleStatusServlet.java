73390d1495a936367724b7bbf46bc10a9b00cccd
bala-amypo <bala@amypo.in>
2025-12-17 05:22:26 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -4,4 +4,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.*;
 import jakarta.io.IOException;
 
-@WebServlet(urlpattern = ("/status"))
+@WebServlet(urlPatterns = ("/status"))
+public class SimpleStstusServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req , HttpServletResponse res)throws IOException{
+        res.setStatus(HttpServletResponse.SC_Ok);
+        res.getWriter().writer("Servlet Running");
+    }
+}
