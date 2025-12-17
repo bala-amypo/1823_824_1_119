2dc89b2957edf86ebf341943ceb3f0b69fd4b573
bala-amypo <bala@amypo.in>
2025-12-17 06:40:26 +0000

    User commit

--- src/main/java/com/example/demo/model/stock.java
+++ src/main/java/com/example/demo/model/stock.java
@@ -1,8 +1,15 @@
 
-import jakarta.
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class stock{
-
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  private long id;
+  @column(unique=true)
+  private String ticker;
+  private String companyName;
+  private String sector;
+  private boolean active
 }
