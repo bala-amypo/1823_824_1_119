0e9e2a585fe93a3c99c54f97b18eaa0b1be0f2b7
bala-amypo <bala@amypo.in>
2025-12-17 09:34:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/userportfolio.java
@@ -0,0 +1,60 @@
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+@Entity
+public class userportfolio{
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  private Long id;
+  private Long userId;
+  @Column(unique=true)
+  private String portfolioName;
+  private Boolean active;
+
+  public Long getid(){
+  return id;
+}
+public void setid(Long id){
+  this.id=id;
+}
+public String getticker(){
+  return ticker;
+}
+public void setticker(String ticker){
+  this.ticker=ticker;
+}
+public String getcompanyName(){
+  return companyName;
+}
+public void setcompanyName(String companyName){
+  this.companyName=companyName;
+}
+public String getsector(){
+  return sector;
+}
+public void setsector(String sector){
+  this.sector=sector;
+}
+public Boolean getactive(){
+  return active;
+}
+public void setactive(Boolean active){
+  this.active=active;
+}
+public stock(Long id , String ticker, String companyName, String sector, Boolean active){
+ this.id=id;
+ this.ticker=ticker;
+ this.companyName=companyName;
+ this.sector=sector;
+ this.active=active;
+}
+public user(){
+
+}
+
+
+}
