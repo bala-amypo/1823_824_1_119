1583cbea626c85d4b715d23178434d3fb2db6b82
bala-amypo <bala@amypo.in>
2025-12-18 05:39:25 +0000

    User commit

--- src/main/java/com/example/demo/service/StockService.java
+++ src/main/java/com/example/demo/service/StockService.java
@@ -1,7 +1,11 @@
 package com.example.demo.service;
 import java.util.List;
-
+import com.example.demo.model.stock;
 
 public interface StockService{
-    
-}
+    Stock createStock(stock stock);
+    Stock updateStock(Long id, stock stock);
+    Stock getStockById(Long id);
+    List<stock> getAllStocks();
+    void deactivateStock(Long id);
+    }
