7b05360c4b4abf9304c04fddf50ca5190a18c9f8
bala-amypo <bala@amypo.in>
2025-12-18 08:00:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StockServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StockServiceImpl.java
@@ -1,1 +1,11 @@
 package com.example.demo.service;
+import com.example.demo.model.Stock;
+import com.example.demo.repository.StockRepository;
+import org.springframework.stereotype.service;
+import java.util.List;
+
+@Service
+public class StockServiceImpl implements StockService{
+    private final StockRepositary stockRepository;
+    public StockServiceImpl
+}
