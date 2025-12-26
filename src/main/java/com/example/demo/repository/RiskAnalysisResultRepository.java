7d76ad923a9bac8ed63aa87a90c173bbcb5cc0ff
bala-amypo <bala@amypo.in>
2025-12-26 09:49:50 +0000

    User commit

--- src/main/java/com/example/demo/repository/RiskAnalysisResultRepository.java
+++ src/main/java/com/example/demo/repository/RiskAnalysisResultRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.repository;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.demo.model.RiskAnalysisResult;
 
-public interface RiskAnalysisResultRepository 
-    extends JpaRepository<RiskAnalysisResult, Long> {
-    List<RiskAnalysisResult> findByPortfolioId(Long portfolioId);
-    List<RiskAnalysisResult> findByRiskLevel(String riskLevel);  // ADD THIS METHOD
+public interface RiskAnalysisResultRepository
+        extends JpaRepository<RiskAnalysisResult, Long> {
+
+    List<RiskAnalysisResult> findByPortfolio_Id(Long portfolioId);
 }
